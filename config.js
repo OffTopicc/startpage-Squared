17b555de1e5c9221d439ee1b7f8fc2f9f11a8d5c
OffTopicc <37567139+OffTopicc@users.noreply.github.com>
2018-06-29 23:51:15 -0400

    re
    re

--- config.js
+++ config.js
@@ -47,31 +47,31 @@ var nameB = [
 /*Box 03*/	"Gmail",
 /*Box 04*/	"Reddit" ,
 /*Box 05*/	"DeviantArt" ,
-/*Box 06*/	"Six",
-/*Box 07*/	"Seven",
-/*Box 08*/	"Eight",
+/*Box 06*/	"Twitch",
+/*Box 07*/	"SoundCloud",
+/*Box 08*/	"Yahoo",
 
 //-- Page 2 --//
 
-/*Box 09*/	"Nine",
-/*Box 10*/	"Ten",
-/*Box 11*/	"Eleven",
-/*Box 12*/	"Twelve",
-/*Box 13*/	"Thirteen",
-/*Box 14*/	"Fourteen",
-/*Box 15*/	"Five",
-/*Box 16*/	"Sixteen",
+/*Box 09*/	"KissAnime",
+/*Box 10*/	"Jamini",
+/*Box 11*/	"Bato.to",
+/*Box 12*/	"Nyaa",
+/*Box 13*/	"HorribleSubs",
+/*Box 14*/	"/a/",
+/*Box 15*/	"/w/",
+/*Box 16*/	"/c/",
 
 //-- Page 3 --//
 
-/*Box 17*/	"Seventeen",
-/*Box 18 */	"Eighteen",
-/*Box 19 */	"Nineteen",
-/*Box 20 */	"Twenty",
-/*Box 21 */	"Twenty1",
-/*Box 22 */	"Twenty2",
-/*Box 23 */	"Twenty3",
-/*Box 24 */	"Twenty4",
+/*Box 17*/	"Pixiv",
+/*Box 18 */	"SankakuComplex",
+/*Box 19 */	"IdolComplex",
+/*Box 20 */	"EX",
+/*Box 21 */	"Hitomi",
+/*Box 22 */	"8ch",
+/*Box 23 */	"/h/",
+/*Box 24 */	"Tumblr",
 
 //-- Page 4 --//
 
@@ -107,36 +107,36 @@ var linkB = [
 
 //-- Page 1 --//
 
-/*Box 01*/	"",
-/*Box 02*/	"",
-/*Box 03*/	"",
-/*Box 04*/	"",
-/*Box 05*/	"",
-/*Box 06*/	"",
-/*Box 07*/	"",
-/*Box 08*/	"",
+/*Box 01*/	"https://www.youtube.com/",
+/*Box 02*/	"https://twitter.com/",
+/*Box 03*/	"https://mail.google.com/mail/u/0/#inbox",
+/*Box 04*/	"https://www.reddit.com/",
+/*Box 05*/	"https://www.deviantart.com/",
+/*Box 06*/	"https://www.twitch.tv/",
+/*Box 07*/	"https://soundcloud.com/stream",
+/*Box 08*/	"https://mail.yahoo.com/d/folders/1",
 
 //-- Page 2 --//
 
-/*Box 09*/	"",
-/*Box 10*/	"",
-/*Box 11*/	"",
-/*Box 12*/	"",
-/*Box 13*/	"",
-/*Box 14*/	"",
-/*Box 15*/	"",
-/*Box 16*/	"",
+/*Box 09*/	"http://kissanime.ru/",
+/*Box 10*/	"https://jaiminisbox.com/",
+/*Box 11*/	"https://bato.to/",
+/*Box 12*/	"https://nyaa.si/",
+/*Box 13*/	"https://horriblesubs.info/",
+/*Box 14*/	"https://boards.4chan.org/a/",
+/*Box 15*/	"https://boards.4chan.org/w/",
+/*Box 16*/	"https://boards.4chan.org/c/",
 
 //-- Page 3 --//
 
-/*Box 17*/	"",
-/*Box 18 */	"",
-/*Box 19 */	"",
-/*Box 20 */	"",
-/*Box 21 */	"",
-/*Box 22 */	"",
-/*Box 23 */	"",
-/*Box 24 */	"",
+/*Box 17*/	"https://www.pixiv.net/",
+/*Box 18 */	"https://chan.sankakucomplex.com/",
+/*Box 19 */	"https://idol.sankakucomplex.com/",
+/*Box 20 */	"http://exhentai.org/",
+/*Box 21 */	"https://hitomi.la/",
+/*Box 22 */	"https://8ch.net/index.html",
+/*Box 23 */	"https://boards.4chan.org/h/",
+/*Box 24 */	"https://www.tumblr.com/dashboard",
 
 //-- Page 4 --//
 
